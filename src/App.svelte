6b6f01cb94866c0605d7b11c31af9e7aae2e49cb
Mathis Laroche <ecomath360@gmail.com>
2022-09-14 08:42:06 -0400

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+    <h1>
+        Démos du cours <em>IFT1016</em>
+    </h1>
+    <section>
+
+    </section>
+    <section>
+
+    </section>
+    <section>
+
+    </section>
+    <section>
+
+    </section>
+    <section>
+
+    </section>
+</main>
+
+<style>
+    main {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
